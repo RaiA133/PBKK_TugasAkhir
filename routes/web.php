d383c09b74c5d4d9ccd911368a8d80d6e2879bab
islamiyanti123 <armyelsaislamiyanti777@gmail.com>
2023-06-25 12:11:46 +0700

    use

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
